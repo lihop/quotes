16d8d566349f21c47866e8eff617e3a6247594b3
Leroy Hopson <git@leroy.geek.nz>
2024-09-03 22:14:52 +1200

    Add bond funds

--- cypress/e2e/sharesight.cy.js
+++ cypress/e2e/sharesight.cy.js
@@ -1,8 +1,11 @@
 // SPDX-FileCopyrightText: 2022 Leroy Hopson <copyright@leroy.geek.nz>
 //
 // SPDX-License-Identifier: CC0-1.0
+const HOLDINGS = [25445, 25870, 25871];
 const FUNDS = {
-  "Kernel Cash Plus Fund": "FND40600.NZ"
+  "Kernel Cash Plus Fund": "FND40600.NZ",
+  "Kernel March 2027 NZ Bond Fund": "FND46943.NZ",
+  "Kernel March 2029 NZ Bond Fund": "FND46944.NZ"
 };
 
 const concat = require("lodash/concat");
@@ -23,36 +26,44 @@ describe("", () => {
       }
     );
     cy.get('input[type="submit"]').click();
-    cy.get('[data-cy="HoldingName-25445"]').click();
-    cy.intercept("https://*/charts/instrument_price_data.json?*").as("charts");
-    cy.wait("@charts", { timeout: 120000 }).then(interception => {
-      cy.get('span[class^="DulledTextstyled"]').then(el => {
-        const lastUpdated = moment(
-          el.text().replace(" Updated at ", ""),
-          "DD MMMM YYYY, h:mm A Z"
-        );
+    const allQuotes = [];
+    HOLDINGS.forEach(holding => {
+      cy.visit("https://portfolio.sharesight.com/");
+      cy.get('[data-cy="HoldingName-' + holding + '"]').click();
+      cy.intercept("https://*/charts/instrument_price_data.json?*").as(
+        "charts-" + holding
+      );
+      cy.wait("@charts-" + holding, { timeout: 120000 }).then(interception => {
+        cy.get('span[class^="DulledTextstyled"]').then(el => {
+          const lastUpdated = moment(
+            el.text().replace(" Updated at ", ""),
+            "DD MMMM YYYY, h:mm A Z"
+          );
 
-        const dates = get(interception, "response.body.graph.xAxis.categories");
+          const dates = get(
+            interception,
+            "response.body.graph.xAxis.categories"
+          );
 
-        let allQuotes = [];
-        get(interception, "response.body.graph.series").forEach(fund => {
-          if (keys(FUNDS).includes(fund.name)) {
-            const quotes = get(fund, "data", [])
-              .map((quote, i) => ({
-                symbol: FUNDS[fund.name],
-                date: moment(dates[i], "DD MMM YY"), //.format("YYYY-MM-DD"),
-                price: parseFloat(quote.y2)
-              }))
-              .filter(quote => quote.date.isSameOrBefore(lastUpdated))
-              .map(quote => ({
-                ...quote,
-                date: quote.date.format("YYYY-MM-DD")
-              }));
-            allQuotes = concat(allQuotes, quotes);
-          }
+          get(interception, "response.body.graph.series").forEach(fund => {
+            if (keys(FUNDS).includes(fund.name)) {
+              const quotes = get(fund, "data", [])
+                .map((quote, i) => ({
+                  symbol: FUNDS[fund.name],
+                  date: moment(dates[i], "DD MMM YY"),
+                  price: parseFloat(quote.y2)
+                }))
+                .filter(quote => quote.date.isSameOrBefore(lastUpdated))
+                .map(quote => ({
+                  ...quote,
+                  date: quote.date.format("YYYY-MM-DD")
+                }));
+              quotes.forEach(quote => allQuotes.push(quote));
+            }
+          });
         });
-        cy.writeFile("kernel_quotes.json", allQuotes);
       });
     });
+    cy.writeFile("kernel_quotes.json", allQuotes);
   });
 });
